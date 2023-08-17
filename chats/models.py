1f9133a76941482216b55532c89ea5fa353aa983
acacio maximiliano <maxyacacio@gmail.com>
2023-08-17 15:52:58 +1030

    one

---
+++ chats/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Chats(models.Model):
+    author = models.CharField(max_length=250, null=True)
+    sender = models.CharField(max_length=250, null=True)
+    name = models.TextField()
+
+
+
+    def __str__(self):
+        return self.author
+
+# Create your models here.
