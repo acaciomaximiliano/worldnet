e68ed83e14c4c1c2138908ad736a7f0fbbf6b2a1
Acácio Maximiliano <127418194+acaciomaximiliano@users.noreply.github.com>
2023-08-17 23:47:41 +1030

    Update models.py
    Agora 150

--- chats/models.py
+++ chats/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Chats(models.Model):
     author = models.CharField(max_length=250, null=True)
-    sender = models.CharField(max_length=100, null=True)
+    sender = models.CharField(max_length=150, null=True)
     name = models.TextField()
 
 
