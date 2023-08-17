ee87cdc59a7de547e0076b11c396a4df15cde564
acacio maximiliano <maxyacacio@gmail.com>
2023-08-17 16:05:56 +1030

    2nd round

--- chats/models.py
+++ chats/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Chats(models.Model):
     author = models.CharField(max_length=250, null=True)
-    sender = models.CharField(max_length=250, null=True)
+    sender = models.CharField(max_length=200, null=True)
     name = models.TextField()
 
 
